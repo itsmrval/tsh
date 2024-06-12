f70cb4a2ecd3fd857828cd677d0e2df075b70874
Valentin <43043885+itsmrval@users.noreply.github.com>
2024-06-12 11:08:56 +0200

    Update debian.sh

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -20,7 +20,7 @@ fi
 
 echo "[2/2] Downloading config file..."
 rm -rf ~/.tmate.conf 
-wget -qO ~/.tmate.conf https://raw.githubusercontent.com/itsmrval/tsh/main/.tmate.conf
+wget -qO ~/.tmate.conf https://raw.githubusercontent.com/itsmrval/tsh/main/config/.tmate.conf
 
 if [ -f ~/.tmate.conf ]; then
     echo "[OK] config installed"
