febff6dbaa6b58fdf2ebf08084c00c00696b8b81
Valentin <43043885+itsmrval@users.noreply.github.com>
2024-07-10 13:37:18 +0200

    Create paste.sh

---
+++ paste.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <fichier>"
+    exit 1
+fi
+
+if [ ! -f "$1" ]; then
+    echo "Erreur: Le fichier $fichier n'existe pas."
+    exit 1
+fi
+
+response=$(curl -X POST -s "https://paste.vpws.eu/documents" --data-binary @"$1")
+
+if [ $? -eq 0 ]; then
+    key=$(echo "$response" | jq -r .key)
+    echo "https://paste.vpws.eu/$key"
+else
+    echo "Une erreur s'est produite lors du téléchargement."
+fi
